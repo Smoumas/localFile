00b1aadad42e99577e19b468fd05a5b72a531a2e
Smoumas <aa497229845@qq.com>
2020-05-17 17:28:24 +0800

    first commit

---
+++ src/main/java/com/file/demo/controller/UploadController.java
@@ -0,0 +1,102 @@
+package com.file.demo.controller;
+
+import com.fasterxml.jackson.databind.util.ClassUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UploadController {
+
+    @RequestMapping(value = "/uploadPage",method = RequestMethod.GET)
+    public String uploadPage(){
+        return "upload";
+    }
+
+    @RequestMapping(value= "/success",method = RequestMethod.GET)
+    public String successPage(){
+        return "success";
+    }
+
+    @RequestMapping(value="/uploadText",method = RequestMethod.GET)
+    public String uploadTextPage(){
+        return "uploadText";
+    }
+
+    @RequestMapping(value="/fail",method = RequestMethod.GET)
+    public String failPage(){
+        return "fail";
+    }
+
+    @RequestMapping(value="/videoPage/{videoName}")
+    public String video(@PathVariable("videoName")String fileName,Model model){
+        model.addAttribute("videoName",fileName);
+        return "videoPage";
+    }
+
+//    @RequestMapping(value = "/videoPage")
+//    public String videoPage(){
+//        return "/video";
+//    }
+
+    @RequestMapping(value="/videoList",method = RequestMethod.GET)
+    public String videoList(Model model){
+        String path = "D:/localVideo/";
+        File videoDir = new File(path);
+        File []videos = videoDir.listFiles();
+        List<String> fileList = new ArrayList<String>();
+        for(File videoFile:videos){
+            fileList.add(videoFile.getName());
+        }
+        model.addAttribute("list",fileList);
+        return "videoList";
+    }
+
+    @RequestMapping(value="/uploadContent",method = RequestMethod.POST)
+    public String uploadContent(String text){
+        File file = new File("D:\\localUpload\\content.txt");
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+            bufferedWriter.write(text);
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+            return "redirect:/fail";
+        }catch(IOException e){
+            e.printStackTrace();
+            return "redirect:/fail";
+        }finally{
+            if(bufferedWriter != null){
+                try {
+                    bufferedWriter.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                    return "redirect:/fail";
+                }
+            }
+        }
+        return "redirect:/success";
+
+    }
+
+    @RequestMapping(value = "/upload",method = RequestMethod.POST)
+    public String upload(MultipartFile file){
+        String fileName = file.getOriginalFilename();
+        String filePath = "D:\\localUpload\\";
+        File dest = new File(filePath+fileName);
+        try {
+            file.transferTo(dest);
+            return "redirect:/success"; //不使用redirect会报错，因为html不支持响应头带有post的应答包
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return "redirect:/fail";
+    }
+}
